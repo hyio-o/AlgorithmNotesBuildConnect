342ed7b51a0595cfecdcafb43d8f84a918697337
chenzhiyu <18172755902@163.com>
2023-08-23 12:59:25 +0800

    8-23:为了尽快结束双指针的训练，Windows同步

--- 9-算法珠玑/1-数组/a-快慢指针/Remove Duplicates from Sorted Array.h
+++ 9-算法珠玑/1-数组/a-快慢指针/Remove Duplicates from Sorted Array.h
@@ -1,18 +1,19 @@
 #include "../../headd.h"
 
-class Solution{
+// Remove Duplicates from Sorted Array
+// Time Complexity: O(n)，Space Complexity: O(1)
+class Solution {
 public:
-    int removeDuplicates(vector<int>& nums)
-    {
-        if(nums.empty()) return 0;
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
 
-        for (int fast = 0, slow = 0; fast < nums.size(); ++fast) {
-            if(nums[slow] != nums[fast])
-            {
-                nums[slow] = nums[fast];
-                slow++;
+        int slow = 0;
+        for (int fast = 0; fast < nums.size(); fast++) {
+            if (nums[fast] != nums[slow]) {
+                nums[++slow] = nums[fast];
             }
         }
+        return slow+1;
     }
 };
 
@@ -21,7 +22,7 @@ public:
  * slow， fast；
  * 1 1 2 3 3 4 5
  * 如果fast和slow是对等的值， 让fast 覆盖 slow ；
- * 如果不对等，
+ * 如果不对等，////////////
  * */
 
 
