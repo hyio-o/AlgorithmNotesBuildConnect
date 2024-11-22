7b85b7b8f923c5748db68708d695029ac60d8bda
chenzhiyu <18172755902@163.com>
2024-11-22 19:34:07 +0800

    241122，为了同步macOS新老

---
+++ 12-左神程序员代码面试指南/2-链表问题/1-1星难度/2-反转部分单向链表.h
@@ -0,0 +1,20 @@
+// 反转部分单向链表
+//给定一个单向链表的头节点 head，以及两个整数 from 和 to，在单向链表上把第 from 个节 点到第 to 个节点这一部分进行反转。
+
+// 1->2->3->4->5->null，from=2，to=4 调整结果为:1->4->3->2->5->null
+
+// 1. 是否满足 1<= from <= to <= N, 若不满足则返回原来头结点。
+// 2. pin住 from-1 为 fPre； pin住 to+1 为 tPos。
+//		（fPre为反转后的的前队链，tPos为反转后的后对
+
+
+
+
+//
+// Created by 陈至宇 on 2024/11/21.
+//
+
+#ifndef ALGORITHMNOTESBUILDING_2_反转部分单向链表_H
+#define ALGORITHMNOTESBUILDING_2_反转部分单向链表_H
+
+#endif //ALGORITHMNOTESBUILDING_2_反转部分单向链表_H
